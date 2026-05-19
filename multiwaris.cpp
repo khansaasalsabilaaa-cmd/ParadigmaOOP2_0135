ff60702ec80023b0d39f557379912a3592ec3b16
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-19 18:21:54 +0700

    menambahkan class orang multiwaris

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,12 +11,5 @@ public:
     }
 };
 
-class pekerja : virtual public orang {
-public:
-    pekerja(int pUmur)
-        orang(pUmur)
-    {
-        cout << "pekerja dibuat" << endl;
-    }
-};
+
 
