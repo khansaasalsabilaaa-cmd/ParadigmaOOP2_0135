63f5be6614963706325c8a8743c877530e4fe389
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-19 18:16:43 +0700

    menambahkan class base Class final

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 class orang {
-publi
-}
+public:
+    int umur;
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur)
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat" << endl;
+    }
+};
+
